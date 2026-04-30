f0c019a435f4bb0b59420c307e89d994288079d6
tugshinde <tugshinde@gmail.com>
2026-04-30 13:51:18 +0530

    assignment

---
+++ WPT/practic_exam/pss.js
@@ -0,0 +1,13 @@
+let car = {
+    brand: "BMW",
+    model: "X5",
+    year: 2023
+};
+
+let { brand, model, year } = car;
+
+console.log(brand, model, year);let numbers = [10, 20, 30, 40];
+
+let [first, second] = numbers;
+
+console.log(first, second);
